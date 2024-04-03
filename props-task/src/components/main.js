03a32a70ab5cf45f9779c89aa40bf9ddde5a83c3
Roman Petlitksiy <jeremy_wade@mail.ru>
2024-04-03 13:31:17 +0200

    add more names in the list

--- props-task/src/components/main.js
+++ props-task/src/components/main.js
@@ -1,6 +1,6 @@
 import { List } from "./list";
 
 export const Main = () => {
-    const list = ["Anna", "Roman", "George"];
+    const list = ["Anna", "Roman", "George", "New One", "Clark"];
     return <List employees={list} />;
 };
