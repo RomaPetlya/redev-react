30de2fb88c5c5109e2ef12bed6512e782f82c6d8
Roman Petlitksiy <jeremy_wade@mail.ru>
2024-04-03 21:58:10 +0200

    add keys

--- props-task/src/components/list.js
+++ props-task/src/components/list.js
@@ -1,7 +1,7 @@
 export const List = ({ employees }) => {
     return (
         <ul style={{ width: 10 + "%", listStyleType: "none" }}>
-            {employees.map(item => <li>{item}</li>)}
+            {employees.map((item, index )=> <li key={index}>{item}</li>)}
         </ul>
     );
 };
